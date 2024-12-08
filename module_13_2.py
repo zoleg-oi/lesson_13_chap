7e50af54b610b5d9d336f47f45b02c2421357c47
zoleg-oi <zoleg@mail.ru>
2024-12-08 11:58:58 +0300

    Add files via upload

--- module_13_2.py
+++ module_13_2.py
@@ -2,13 +2,19 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-api = ''
+api = '7618039601:AAHs_XcJDT8_WC65T3cTeHWSYFI58GxI6Qo'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-
-
+@dp.message_handler(text=['Привет!', 'Отклонение'])
+async def text_message(message: types.message):
+    if message.text == 'Привет!':
+        await message.answer('Привет!')
+        print('Привет!')
+    elif message.text == 'Отклонение':
+        await message.answer('Это отклонение от пути')
+        print('Это отклонение от пути')
 
 @dp.message_handler(commands=['start'])
 async def command_start(message):
