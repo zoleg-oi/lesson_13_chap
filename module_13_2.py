9ad074ea79a2f4b94e56dda9aef3019652c8ce39
zoleg-oi <zoleg@mail.ru>
2024-12-08 11:37:17 +0300

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,23 @@
+# Бот Телеги
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+
+
+
+@dp.message_handler(commands=['start'])
+async def command_start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def other_message(message):
+    print("Введите команду /start, чтобы начать общение. ")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
